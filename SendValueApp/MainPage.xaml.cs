33b689fe064e38a07018fbae2cab774bb7316381
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-03-11 22:01:37 -0400

    calculo de  dois entre telas

---
+++ SendValueApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SendValueApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Calcular(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PageCalc(N1.Text, N2.Text));
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SendValueApp/PageCalc.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SendValueApp
+{
+    public partial class PageCalc : ContentPage
+    {
+        public PageCalc(string Valor1, string Valor2)
+        {
+            InitializeComponent();
+        }
+    }
+}
